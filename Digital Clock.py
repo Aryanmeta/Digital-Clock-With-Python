743a61bed2ba9f9489dfaf44b5f14cc53d23973c
aryan zarei <aryan2020.zarei@gmail.com>
2022-10-09 23:01:04 +0330

    Initializing Project

---
+++ Digital Clock.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title('Digital Clock')
+
+def time():
+    string1 = strftime('%r')
+    label.config(text=string1)
+    label.after(1000,time)
+
+label = Label(root,font=('A Iranian Sans',40),background='black',foreground='pink')
+label.pack(anchor='center')
+
+time()
+mainloop()
