4a2687c3d4cee01462c86f8b2ba4e29240987a5a
aryan zarei <aryan2020.zarei@gmail.com>
2022-10-09 23:06:10 +0330

    Update strftime count day

--- Digital Clock.py
+++ Digital Clock.py
@@ -6,7 +6,7 @@ root = Tk()
 root.title('Digital Clock')
 
 def time():
-    string1 = strftime('%F\n%r\n%c')
+    string1 = strftime('%F\n%r\n%c\n%j Day')
     label.config(text=string1)
     label.after(1000,time)
 
